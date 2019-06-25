6adbee5d929ea0c9b6c1679a702a04f0a04ddfdb
Jfleming <Jfleming07712@gmail.com>
2019-06-25 17:19:03 -0400

    Exercise 2 complete.  Have not tried to write a unit test for it, but I assume im going to have the same issue as with Exercise 1.

---
+++ Exercise2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Exercise2
+{
+    //2. Write a C# Sharp program to print the sum of two numbers.
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var x = 10;
+            var y = 20;
+
+            var result = x + y;
+
+            Console.WriteLine(result);
+
+        }
+    }
+}
