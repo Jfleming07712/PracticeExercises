7bbf74346075705313bf00d1107d609e80b916d5
Jfleming <Jfleming07712@gmail.com>
2019-07-11 19:19:18 -0400

    Exercise 7 completed.  I used the code from exercise 6 and gave it more features to achieve the goal in exercise 7.

---
+++ Exercise7/Math.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercise7
+{
+    public class Math
+    {
+        public double MultiplyThreeNumbers(NumberContainer container)
+        {
+            container.Result = container.Number1 * container.Number2 * container.Number3;
+            return container.Result;
+        }
+
+        public double AddTwoNumbers(NumberContainer container)
+        {
+            container.Result1 = container.Number1 + container.Number2;
+            return container.Result;
+        }
+
+        public double SubtractTwoNumbers(NumberContainer container)
+        {
+            container.Result2 = container.Number1 - container.Number2;
+            return container.Result;
+        }
+
+        public double MultiplyTwoNumbers(NumberContainer container)
+        {
+            container.Result3 = container.Number1 * container.Number2;
+            return container.Result;
+        }
+
+        public double DivideTwoNumbers(NumberContainer container)
+        {
+            container.Result4 = container.Number1 / container.Number2;
+            return container.Result;
+        }
+
+        public double ModTwoNumbers(NumberContainer container)
+        {
+            container.Result5 = container.Number1 % container.Number2;
+            return container.Result;
+        }
+
+
+    }
+}
---
+++ Exercise7/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Exercise7
+{
+    public class Program
+    {
+        //Write a C# Sharp program to print on screen the output of adding, subtracting, multiplying and dividing of 
+        //two numbers which will be entered by the user. Go to the editor
+        //Test Data:
+        //Input the first number: 25
+        //Input the second number: 4
+        //Expected Output:
+        //25 + 4 = 29
+        //25 - 4 = 21
+        //25 x 4 = 100 
+        //25 / 4 = 6
+        //25 mod 4 = 1
+
+        
+        
+        
+
+
+
+        public static void Main(string[] args)
+        {
+            NumberContainer container = new NumberContainer();
+            Math doMath = new Math();
+            UserInterface userInterface = new UserInterface();
+
+            userInterface.GatherNumbers(container);
+
+            doMath.AddTwoNumbers(container);
+            doMath.SubtractTwoNumbers(container);
+            doMath.MultiplyTwoNumbers(container);
+            doMath.DivideTwoNumbers(container);
+            doMath.ModTwoNumbers(container);
+
+            userInterface.DisplayAnswer(container);
+        }
+    }
+}
