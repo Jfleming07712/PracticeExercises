425391cb223d8a4e07ea6d5fb08577616525c8b5
Jfleming <Jfleming07712@gmail.com>
2019-07-11 20:07:24 -0400

    Begining exercise 8

---
+++ Exercise8/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Exercise8
+{
+    public class Program
+    {
+        
+
+
+
+        public static void Main(string[] args)
+        {
+            NumberContainer container = new NumberContainer();
+            Math doMath = new Math();
+            UserInterface userInterface = new UserInterface();
+
+            userInterface.GatherNumbers(container);
+
+            doMath.AddTwoNumbers(container);
+            doMath.SubtractTwoNumbers(container);
+            doMath.MultiplyTwoNumbers(container);
+            doMath.DivideTwoNumbers(container);
+            doMath.ModTwoNumbers(container);
+
+            userInterface.DisplayAnswer(container);
+        }
+    }
+}
---
+++ Exercise8/UserInterface.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercise8
+{
+    public class UserInterface
+    {
+        public void GatherNumbers(NumberContainer container)
+        {
+            var done = false;
+            bool valid = false;
+            while (valid == false)
+            {
+                Console.WriteLine("enter your first number");
+                var stringNumber1 = Console.ReadLine();
+
+                if (!double.TryParse(stringNumber1, out double n1))
+                {
+                    Console.WriteLine("not an int");
+                }
+                else
+                {
+                    container.Number1 = n1;
+                    valid = true;
+                }
+            }
+
+            bool valid2 = false;
+            while (valid2 == false && done == false)
+            {
+                Console.WriteLine("enter your second number or enter done");
+                var stringNumber2 = Console.ReadLine();
+
+                if (stringNumber2 == "done")
+                {
+                    done = true; 
+                }
+                else
+                {
+                    if (!double.TryParse(stringNumber2, out double n2))
+                    {
+                        Console.WriteLine("not an int");
+                    }
+                    else
+                    {
+                        container.Number2 = n2;
+                        valid2 = true;
+                    }
+
+                    
+                }
+            }
+
+            bool valid3 = false;
+            while (valid3 == false && done == false)
+            {
+                Console.WriteLine("enter your third number or enter done.");
+                var stringNumber3 = Console.ReadLine();
+
+                if (stringNumber3 == "done")
+                {
+                    done = true;
+                }
+                else
+                {
+                    if (!double.TryParse(stringNumber3, out double n3))
+                    {
+                         Console.WriteLine("not an int");
+                    }
+                    else
+                    {
+                        container.Number3 = n3;
+                        valid3 = true;
+                    }
+
+                    
+                }
+            }
+
+        }
+
+        public void DisplayAnswer(NumberContainer container)
+        {
+            Console.WriteLine( container.Number1 + " + " + container.Number2 + " = " + container.Result1);
+            Console.WriteLine(container.Number1 + " - " + container.Number2 + " = " + container.Result2);
+            Console.WriteLine(container.Number1 + " * " + container.Number2 + " = " + container.Result3);
+            Console.WriteLine(container.Number1 + " / " + container.Number2 + " = " + container.Result4);
+            Console.WriteLine(container.Number1 + " Mod " + container.Number2 + " = " + container.Result5);
+        }
+
+    }
+}
